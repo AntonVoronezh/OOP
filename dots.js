48f75bda19ade3995f1bee1b07da6fa8a0d2dac6
AntonVoronezh <pstut@mail.ru>
2019-02-20 19:57:34 +0300

    settings

---
+++ dots.js
@@ -0,0 +1,37 @@
+(function dots() {
+
+    // const dots = document.querySelector('#dots');
+
+    const settings = {
+
+        dots: document.querySelector('#dots'),
+        row: 30,
+        col: 30,
+        table_class: {
+            table: 'dots__table',
+            row: 'dots__tr',
+            col: 'dots__td'
+        },
+        gamers: {
+            gamer1: 'gamer1',
+            gamer2: 'gamer2'
+
+        },
+        occupied: 'occupied'
+
+    };
+
+
+
+
+
+
+
+
+
+
+
+
+    new Game(settings.dots, settings.row, settings.col);
+
+})();   
