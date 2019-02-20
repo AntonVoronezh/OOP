6db3a017175a27b2faf048ccb17bda48211ced5a
AntonVoronezh <pstut@mail.ru>
2019-02-20 19:59:00 +0300

    _makeColor

--- dots.js
+++ dots.js
@@ -92,7 +92,19 @@
 
         }
 
+        _makeColor() {
 
+            if (!this._elem.classList.contains(settings.occupied)) {
+
+                let cl = this._queue.getNextGamer();
+
+                this._elem.classList.add(cl);
+
+                this._elem.classList.add(settings.occupied);
+
+            }
+
+        }
 
 
 
