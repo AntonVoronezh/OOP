b3fed3b325ee80634feeafe0141181ee5e2a045a
AntonVoronezh <pstut@mail.ru>
2019-02-22 23:28:19 +0300

    settings

---
+++ stickers.js
@@ -0,0 +1,25 @@
+(function stickers() {
+
+    const settings = {
+
+        notes: document.querySelector('#stickers'),
+        sticker: 'sticker',
+        sticker_settings: {
+            top: 0,
+            left: 0
+        }
+
+    };
+
+
+
+
+
+
+
+
+
+
+    new Notes();
+
+})();
