8a79a9334ddcd432cb465be1231c4cb3fcfa362d
AntonVoronezh <pstut@mail.ru>
2019-03-28 15:16:54 +0300

    dropdown

---
+++ select.js
@@ -0,0 +1,20 @@
+
+const dropdown = new Dropdown('#dropdown', {
+    items: [{
+            label: 'Москва',
+            id: 'msk'
+        },
+        {
+            label: 'Санкт-Петербург',
+            id: 'spb'
+        },
+        {
+            label: 'Новосибирск',
+            id: 'nsk'
+        },
+        {
+            label: 'Краснодар',
+            id: 'krdr'
+        }
+    ]
+});
